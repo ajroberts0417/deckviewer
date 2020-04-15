c6dc070321947d342e14850c19548fe2430bc64c
ajroberts0417 <ajroberts0417@gmail.com>
2020-04-15 04:12:16 -0700

    initial commit, WIP airtable integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Airtable from 'airtable';
+
 function App() {
+
+  const [deck, setDeck] = useState([]);
+  console.log("REACT STATE DECK: ", deck)
+
+  useEffect(() => {
+    // airtable
+    const base = new Airtable({ apiKey: 'XXXXXXXXX' }).base('appr7aUJe07RDR9Ho');
+    
+    let myDeck = []
+
+    base('Card').select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 10,
+      view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+  
+      records.forEach(function(record) {
+        myDeck.push(record)
+        console.log('Retrieved', record);
+      });
+  
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+  
+    }, function done(err) {
+      if (err) { console.error(err); return; }
+
+      console.log(myDeck);
+      setDeck(myDeck);
+    });
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +56,8 @@ function App() {
         >
           Learn React
         </a>
+        <div>blah blah blah</div>
+        {deck.map((card) => <div>{JSON.stringify(card.fields)}</div>)}
       </header>
     </div>
   );
