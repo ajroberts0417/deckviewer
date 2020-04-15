2746444f6d24b5d44f0267ec3fe00f9eb948f629
ajroberts0417 <ajroberts0417@gmail.com>
2020-04-15 05:03:38 -0700

    new configuration + WIP fetch in APP.js

--- src/App.js
+++ src/App.js
@@ -2,42 +2,18 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Airtable from 'airtable';
-
 function App() {
 
   const [deck, setDeck] = useState([]);
   console.log("REACT STATE DECK: ", deck)
 
-  useEffect(() => {
-    // airtable
-    const base = new Airtable({ apiKey: 'XXXXXXXXX' }).base('appr7aUJe07RDR9Ho');
-    
+  useEffect(() => {  
     let myDeck = []
 
-    base('Card').select({
-      // Selecting the first 3 records in Grid view:
-      maxRecords: 10,
-      view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-      // This function (`page`) will get called for each page of records.
-  
-      records.forEach(function(record) {
-        myDeck.push(record)
-        console.log('Retrieved', record);
-      });
-  
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      fetchNextPage();
-  
-    }, function done(err) {
-      if (err) { console.error(err); return; }
+    const myFunction = async () =>
+    await (await fetch('/.netlify/functions/my-function/my-function.js')).json();
 
-      console.log(myDeck);
-      setDeck(myDeck);
-    });
+    console.log(myFunction);
 
   }, [])
 
