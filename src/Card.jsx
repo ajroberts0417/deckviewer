a9946edccadb899618d30a2d83a82b17156ce4bd
ajroberts0417 <ajroberts0417@gmail.com>
2020-04-17 03:41:09 -0700

    line buttons up into a column

--- src/Card.jsx
+++ src/Card.jsx
@@ -14,7 +14,7 @@ const useStyles = makeStyles({
     color: 'white',
     height: '10%',
     padding: '0 4px',
-    margin: '0px',
+    margin: '2px 0',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
   },
   label: {
@@ -57,17 +57,17 @@ const classes = useStyles();
         <Typography variant="body2">
           {rulesText}
         </Typography>
-        <CardActions disableSpacing>
+        <CardActions className="card-button-area"disableSpacing>
           <Button size="small" color="primary" classes={{root: classes.root, label: classes.label}}>
             Draw
           </Button>
           <Button classes={{root: classes.root, label: classes.label}} size="small" color="primary">
             Play
           </Button>
-        </CardActions>
           <Button classes={{root: classes.root, label: classes.label}} size="small" color="primary">
             Discard
           </Button>
+        </CardActions>
         {children}
       </MuiCard>
   );
