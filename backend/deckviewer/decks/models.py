33cca325c3f9c507e789cb910ea6e16dc246795b
ajroberts0417 <ajroberts0417@gmail.com>
2020-05-05 18:29:59 -0700

    add a playercard model

--- backend/deckviewer/decks/models.py
+++ backend/deckviewer/decks/models.py
@@ -16,16 +16,22 @@ class DeckCard(models.Model):
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.deck) + "-" + str(self.card)
+        return str(self.deck) + " - " + str(self.card)
 
 
 class Player(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.user)
+
 
 class PlayerCard(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.player.user) + " - " + str(self.card)
 
 
 class Deck(models.Model):
