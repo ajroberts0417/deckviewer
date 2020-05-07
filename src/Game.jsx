c713dc3143ade1cbc783c66f9cd0c39197a62674
ajroberts0417 <ajroberts0417@gmail.com>
2020-05-06 23:13:40 -0700

    remove console.log

--- src/Game.jsx
+++ src/Game.jsx
@@ -58,7 +58,6 @@ const Game = ({deck}) => {
       try {
           const response = await fetch(url);
           const data = await response.json();
-          console.log(data);
           return data;
       } catch (err) {
           console.log(err);
