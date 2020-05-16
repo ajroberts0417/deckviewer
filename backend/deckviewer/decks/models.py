907d4dbf99033c8584d7c8bb9ecc72f87d8480ff
ajroberts0417 <ajroberts0417@gmail.com>
2020-05-15 20:10:04 -0700

    expand model schema and makemigrations

--- backend/deckviewer/decks/models.py
+++ backend/deckviewer/decks/models.py
@@ -5,7 +5,29 @@ from django.contrib.auth import get_user_model
 
 
 class Card(models.Model):
+
+    CLASS_CHOICES = [
+        ('neutral', 'Neutral'),
+        ('wizard', 'Wizard'),
+        ('fighter', 'Fighter'),
+        ('ranger', 'Ranger'),
+        ('cleric', 'Cleric'),
+    ]
+
+    TYPE_CHOICES = [
+        ('permanent', 'Permanent'),
+        ('reaction', 'Reaction'),
+        ('action', 'Action'),
+    ]
+
+    airtable_id = models.CharField(max_length=100, blank=True, null=True)
+    card_type = models.CharField(max_length=100, choices=TYPE_CHOICES)
+    class_type = models.CharField(max_length=100, choices=CLASS_CHOICES)
+    cost = models.IntegerField()
+    designer = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
+    range = models.CharField(max_length=100)
+    rules_text = models.TextField()
 
     def __str__(self):
         return self.name
@@ -35,6 +57,8 @@ class PlayerCard(models.Model):
 
 
 class Deck(models.Model):
+
+    airtable_id = models.CharField(max_length=100, blank=True, null=True)
     name = models.CharField(max_length=100)
     player = models.ForeignKey(Player, on_delete=models.CASCADE, blank=True, null=True)
 
