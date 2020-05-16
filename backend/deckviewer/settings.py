0fd7fa501a696cede1ddbeffa41c98ec086aafbd
ajroberts0417 <ajroberts0417@gmail.com>
2020-05-15 18:40:59 -0700

    add postgres support

--- backend/deckviewer/settings.py
+++ backend/deckviewer/settings.py
@@ -79,12 +79,15 @@ WSGI_APPLICATION = "deckviewer.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDBNAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
