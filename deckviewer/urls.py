36b48e6db30a1f70519bb48d9cc2fa3a876b028c
ajroberts0417 <ajroberts0417@gmail.com>
2020-05-16 20:02:34 -0700

    move all the files

--- deckviewer/urls.py
+++ deckviewer/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from deckviewer.schema import schema
+
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
-
-from deckviewer.schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
