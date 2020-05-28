e2556e665a8696cb060bdf6fdb1c5080484ad11e
ajroberts0417 <ajroberts0417@gmail.com>
2020-05-27 20:32:47 -0700

    setup frontend to use JWTs from localstorage

--- web/src/index.js
+++ web/src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/link-context'
 import { BrowserRouter } from 'react-router-dom'
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token')
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : ''
+    }
+  }
+})
+
+const httpLink = createHttpLink({
+  // eslint-disable-next-line no-undef
+  uri: process.env.NODE_ENV == 'production' ? 'https://deckviewer.herokuapp.com/graphql' : 'http://localhost:8000/graphql',
+})
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    // eslint-disable-next-line no-undef
-    uri: process.env.NODE_ENV == 'production' ? 'https://deckviewer.herokuapp.com/graphql' : 'http://localhost:8000/graphql',
-  })
+  link: authLink.concat(httpLink),
 })
 
 ReactDOM.render(
