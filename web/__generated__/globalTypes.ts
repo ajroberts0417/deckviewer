15d542c08b7becf204e63d527bf5a058fcf9eb6b
ajroberts0417 <ajroberts0417@gmail.com>
2020-05-27 22:35:37 -0700

    update schema and types

---
+++ web/__generated__/globalTypes.ts
@@ -0,0 +1,32 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+/**
+ * An enumeration.
+ */
+export enum CardCardType {
+  ACTION = "ACTION",
+  PERMANENT = "PERMANENT",
+  REACTION = "REACTION",
+}
+
+/**
+ * An enumeration.
+ */
+export enum CardClassType {
+  CLERIC = "CLERIC",
+  FIGHTER = "FIGHTER",
+  NEUTRAL = "NEUTRAL",
+  RANGER = "RANGER",
+  WIZARD = "WIZARD",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
